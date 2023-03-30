14e20d2591f59065a2d644d895c43edc3bd11b3c
Stephen Oba <obastepheno@gmail.com>
2023-03-30 13:54:55 +0100

    ➕ Add annotated function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module contains the function `concat`"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function: concat(str1: str, str2: str) -> str
+
+    Concatenates two strings
+
+    Args
+    ----
+    str1: string
+    str2: string
+
+    Returns
+    -------
+    A concatenation of both strings
+    """
+    return str1 + str2
