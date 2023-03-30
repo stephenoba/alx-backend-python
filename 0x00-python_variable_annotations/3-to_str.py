473b2d18cb9632c41632759c4fa1594043dd5ffd
Stephen Oba <obastepheno@gmail.com>
2023-03-30 14:06:27 +0100

    ➕ Add annotated. function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module contains function `t0_str`"""
+
+
+def to_str(n: float) -> str:
+    """
+    function: to_str(n: float) -> str
+
+    Takes a floating point number and converts to a str
+
+    Args
+    ----
+    n: float
+
+    Returns
+    -------
+    The string representation of the number
+    """
+    return str(n)
