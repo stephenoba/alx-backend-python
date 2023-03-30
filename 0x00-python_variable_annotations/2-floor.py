01c032eeba6cdf2387a264a0bc34e354ee5d7072
Stephen Oba <obastepheno@gmail.com>
2023-03-30 14:01:37 +0100

    ➕ Add annotated function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module contains function `floor`"""
+
+
+def floor(n: float) -> int:
+    """
+    function: floor(n: float) -> int
+
+    Returns the floor of a floating point number
+
+    Args
+    ----
+    n: float
+
+    Returns
+    -------
+    An integer
+    """
+    return int(n)
