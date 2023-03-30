209a9a2ba40edd2f063a8ae234044f812b15bddd
Stephen Oba <obastepheno@gmail.com>
2023-03-30 13:44:37 +0100

    ➕ Add annotated function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+
+Contains function `add`"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function: add(a: float, b: float) -> float
+
+    Adds two floating point numbers
+
+    Args
+    ----
+    a: float
+    b: float
+
+    Returns
+    -------
+    The sum of a and b
+    """
+    return a + b
