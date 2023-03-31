6af9ef5ba96d0b38140b5932a53c280034e4d5ae
Stephen Oba <obastepheno@gmail.com>
2023-03-31 21:57:01 +0100

    ➕ Add file 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module contains function `sum_mixed_list`
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """
+    function: sum_mixed_list(mxd_lst: List[int | float]) -> float
+
+    Takes a list of numbers and sums them
+
+    Args
+    ----
+    mxd_lst: list[int | floats]
+
+    Returns
+    -------
+    Sum of all numbers
+    """
+    return sum(mxd_lst)
