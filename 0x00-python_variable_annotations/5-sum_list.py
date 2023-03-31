c44d4fc1ea0a861765f90569e2a50dbe32da41ee
Stephen Oba <obastepheno@gmail.com>
2023-03-31 21:27:15 +0100

    ➕ Add file 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module contains function `sum_list`
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    function: sum_list(input_list: list[float]) -> float
+
+    Takes a list of floating point number and sums them
+
+    Args
+    ----
+    n: list[floats]
+
+    Returns
+    -------
+    Sum of all numbers
+    """
+    return sum(input_list)
