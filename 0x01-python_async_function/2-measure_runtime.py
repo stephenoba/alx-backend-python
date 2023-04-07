4805015e1ed6a7584a2b2ac6ec6ae38cac4189c4
Stephen Oba <obastepheno@gmail.com>
2023-04-08 00:30:36 +0100

    📝 Annotate variables and return value

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay)
 
