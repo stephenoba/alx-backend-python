a3fdf85eb84edfbdf78b967ed63040bca5d88599
Stephen Oba <obastepheno@gmail.com>
2023-04-07 21:40:21 +0100

    ➕ Add function wait_random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module contains an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random that waits for a
+random delay between 0 and max_delay (included and float value) seconds
+and eventually returns it
+"""
+import random
+import asyncio
+
+
+def wait_random(max_delay: int = 10) -> float:
+    """
+    A simple coroutine.
+
+    Args
+    ----
+    max_delay: int
+
+    Returns
+    -------
+    Randomly generated number (floats included)
+    """
+    rand_num = random.uniform(0, max_delay)
+    asyncio.sleep(rand_num)
+    return rand_num
