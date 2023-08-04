33398193f7e7d81408fdacb6d5546516a38814ee
Stephen Oba <obastepheno@gmail.com>
2023-08-04 13:26:13 +0100

    fix linting error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,10 +10,10 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test Suit for access_nested_map
     """
     @parameterized.expand([
-       ("negative", {"a": 1}, ("a",), 1),
-       ("integer", {"a": {"b": 2}}, ("a",), {"b": 2}),
-       ("large fraction", {"a": {"b": 2}}, ("a", "b"), 2),
-   ])
+       ("a", {"a": 1}, ("a",), 1),
+       ("b", {"a": {"b": 2}}, ("a",), {"b": 2}),
+       ("c", {"a": {"b": 2}}, ("a", "b"), 2),
+    ])
     def test_access_nested_map(self, _, nested_map, path, expected):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
