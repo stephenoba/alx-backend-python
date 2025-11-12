025c1dae55508dce3a98b10a0a7799794e7b1901
Stephen Oba <stephenoba@Stephens-Laptop.local>
2025-11-12 14:24:53 +0100

    0-databaseconnection.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Contains a class based context manager to handle opening and closing database connections automatically.
+"""
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        if self.conn:
+            self.conn.close()
