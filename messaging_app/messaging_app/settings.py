57d58712c4604779fc70b4893e6dc7e4f7800e36
Stephen Oba <stephenoba@Stephens-Laptop.local>
2025-11-27 00:30:21 +0100

    Add settings for 'rest_framework_simplejwt'

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_seed',
     'chats.apps.ChatsConfig',
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from chats.urls import urlpatterns as chat_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("chats.urls"))
+    path("api/", include("chats.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
