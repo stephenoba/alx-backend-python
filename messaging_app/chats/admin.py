50736b38c631676589673fbfba767ae3a0393a38
Stephen Oba <stephenoba@Stephens-Laptop.local>
2025-11-27 00:27:18 +0100

    Add permission class

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import User, Message, Conversation
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_filter = ('role', 'created_at')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'sender_id', 'message_body', 'sent_at')
+    search_fields = ('sender_id__email', 'message_body')
+    list_filter = ('sent_at',)
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('conversation_id', 'participants_id', 'created_at')
+    search_fields = ('participants_id__email',)
+    list_filter = ('created_at',)
+
+
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import User, Message, Conversation
 from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
+from .permissions import IsOwner
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = []
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         user = self.request.user
@@ -19,3 +20,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(sender_id=user)
