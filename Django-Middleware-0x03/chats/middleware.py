929e1683e74adf1b769f8bd380d074bd6d30083c
Stephen Oba <stephenoba@Stephens-Laptop.local>
2025-12-09 22:08:16 +0100

    Add RestrictAccessByTimeMiddleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -14,3 +14,22 @@ class RequestLoggingMiddleware:
 
         response = self.get_response(request)
         return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    """
+    Middleware that restricts access by returning an error 403 Forbidden
+    if a user accesses the chat outside 9PM and 6PM.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        if current_hour < 9 or current_hour > 18:
+            from django.http import HttpResponseForbidden
+            return HttpResponseForbidden("Access to the chat is restricted to between 9AM and 6PM.")
+
+        response = self.get_response(request)
+        return response
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
